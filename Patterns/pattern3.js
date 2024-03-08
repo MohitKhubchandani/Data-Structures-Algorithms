e48d8af232540014dc97a43f968f2305fe0313d3
Mohit04 <mohitkhubchandani88@gmail.com>
2024-03-09 02:08:21 +0530

    Added pattern #3

---
+++ Patterns/pattern3.js
@@ -0,0 +1,16 @@
+// Pattern #3
+
+for(let i = 1; i <= 5; i++){
+    for (let j = 0; j <= 5 - i ; j++) {
+        process.stdout.write("* ");
+        
+    
+    } process.stdout.write("\n");}
+    
+/**
+ * Output :-    * * * * *
+                * * * * 
+                * * *
+                * * 
+                *
+ */
