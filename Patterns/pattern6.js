34a53ee238a2208855f509d5b7a3728a2fe9214e
Mohit04 <mohitkhubchandani88@gmail.com>
2024-03-12 01:00:55 +0530

    Added pattern #6

---
+++ Patterns/pattern6.js
@@ -0,0 +1,45 @@
+// pattern #6
+
+
+for(let i = 0; i <= 4; i++){
+    for (let j = 0; j <= 5-i-1 ; j++ ) {
+        process.stdout.write(" ");
+    } 
+
+    for (let j = 0; j <= 2*i ; j++ ) {
+        process.stdout.write("*");
+    } 
+    
+    
+    for (let j = 0; j <= 5-i-1 ; j++ ) {
+        process.stdout.write(" ");
+    } 
+    process.stdout.write("\n");}
+
+    for(let i = 0; i <= 4; i++){
+        for (let j = 0; j <= i ; j++ ) {
+            process.stdout.write(" ");
+        } 
+    
+        for (let j = 0; j <= 8 - (2*i) ; j++ ) {
+            process.stdout.write("*");
+        } 
+        
+        
+        for (let j = 0; j <= i ; j++ ) {
+            process.stdout.write(" ");
+        } 
+        process.stdout.write("\n");}
+
+ /** 
+Output :-     *     
+             ***    
+            *****   
+           *******  
+          ********* 
+          ********* 
+           *******  
+            *****   
+             ***    
+              *   
+    */
