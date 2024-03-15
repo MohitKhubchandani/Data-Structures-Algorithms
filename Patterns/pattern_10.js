7d98203d53515b64c3f1e252d317b04d0c3a42b6
Mohit04 <mohitkhubchandani88@gmail.com>
2024-03-16 01:12:18 +0530

    Added problem #10

---
+++ Patterns/pattern_10.js
@@ -0,0 +1,23 @@
+// pattern #10
+
+let num = 1
+
+for(let i = 1; i <= 5; i++){
+    
+    for (let j = 1; j <= i; j++) {
+
+        process.stdout.write(`${num.toString()} `)
+        
+        num = num + 1;
+
+    } process.stdout.write("\n");
+}
+
+/** 
+Output :-   1 
+            2 3 
+            4 5 6 
+            7 8 9 10 
+            11 12 13 14 15 
+        
+*/
