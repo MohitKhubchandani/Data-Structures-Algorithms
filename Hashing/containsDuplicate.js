247f67bb8ed14f10027ecdfd73b830600fdab399
Mohit04 <mohitkhubchandani88@gmail.com>
2024-03-29 03:10:55 +0530

    Added a file

---
+++ Hashing/containsDuplicate.js
@@ -0,0 +1,16 @@
+// log true if the numbers are distinct and false if contains duplicated
+var containsDuplicate = function(nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+   if (map[nums[i]]) {
+      map[nums[i]] += 1
+    }
+    else{
+      map[nums[i]] = 1
+    }
+    if (map[nums[i]] > 1) return true
+  }
+  return false;
+}
+
+console.log(containsDuplicate([1,2,3,1]));
