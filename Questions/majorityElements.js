9fa842139a9069287fbc1cdab1aaa3f88495a4e6
Mohit04 <mohitkhubchandani88@gmail.com>
2024-04-16 01:47:31 +0530

    Updated majority elements

--- Questions/majorityElements.js
+++ Questions/majorityElements.js
@@ -1,14 +1,24 @@
-debugger
-let arr = [2,1,1,1,1,2]
-let map = new Map()
-for (let i = 0; i < arr.length; i++) {
-  if(map.has(arr[i])){
-    map.set(arr[i] , map.get(arr[i]) + 1)
+function majorityElements(arr){
+  let n = arr.length;
+  let map = new Map();
+  
+  for(i = 0; i < n; i++){
+   let num = arr[i];
+   if(map.has(num)){
+       map.set(num , map.get(num) + 1)
+   }
+   else{
+       map.set(num ,1)
+   }
   }
-
-  else{
-    map.set(arr[i] , 1)
+  for( const [num , value] of map){
+   if(value > Math.floor(n/2)){
+       return num;
+   }
   }
+  return -1
 }
 
-console.log(map);
+const arr = [2,1,1,1,2];
+const Output = majorityElements(arr)
+console.log(Output); // Output :- 1
