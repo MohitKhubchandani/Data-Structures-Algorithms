f52de67862332cfe4bdd83071a3b82ba682ece5c
Mohit04 <mohitkhubchandani88@gmail.com>
2024-05-10 03:01:14 +0530

    Added lower Bound

---
+++ binarySearch/lowerBound.js
@@ -0,0 +1,21 @@
+debugger
+function lowerBound(arr , target){
+  let n = arr.length
+  let ans = n
+  let low = 0;
+  let high = n - 1;
+  while(low <= high){
+    mid = Math.floor((low + high) / 2);
+    if(arr[mid] >= target) {
+      ans = mid;
+      high = mid - 1
+    }else{
+      low = mid + 1
+    }
+  }
+ return ans;
+}
+
+const arr = [1,2,4,5];
+const Output = lowerBound(arr , 2);
+console.log(Output);
