e60c0f5b625633bdf790ee9fd67e8fefad4d9c35
Mohit04 <mohitkhubchandani88@gmail.com>
2024-06-17 00:14:53 +0530

    length of a Linked List

---
+++ linkedList/traversal.js
@@ -0,0 +1,29 @@
+class Node{
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let linkedList = function(arr){
+  let head = new Node(arr[0]);
+  let mover = head
+  for (let i = 1; i < arr.length; i++) {
+    let temp = new Node(arr[i]);
+    mover.next = temp; 
+    mover = temp; 
+  }
+  return head;
+};
+
+let traverse = function(head){
+  let temp = head;
+  while(temp){
+    console.log(temp.data);
+    temp = temp.next;
+  }
+}
+
+const arr = [12,5,8,7]
+head = linkedList(arr);
+head = traverse(head);// Output :- 12 5 8 7
