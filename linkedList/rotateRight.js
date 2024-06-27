5444e5d1f1b192b301d1a56c5b39781bf012fb0b
Mohit04 <mohitkhubchandani88@gmail.com>
2024-06-28 02:27:48 +0530

    Added rotate right

---
+++ linkedList/rotateRight.js
@@ -0,0 +1,60 @@
+class Node{
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let linkedList = function(arr){
+  let head = new Node(arr[0]);
+  let mover = head
+  for (let i = 1; i < arr.length; i++) {
+    let temp = new Node(arr[i]);
+    mover.next = temp; 
+    mover = temp; 
+  }
+  return head;
+};
+
+function findLength(tail){
+  len = 1;
+  while(tail.next !== null){
+    head = head.next;
+    tail = tail.next;
+    len++
+  }
+  return [len,tail];
+}
+
+function rotateRight(head , k){
+  let temp = head;
+  let counter = 0;
+  let [len, tail] = findLength(temp)
+  if(k % len == 0)return head; 
+  tail.next = head;
+  let equal = k % len 
+  while(temp !== null){
+    if(counter == equal){
+      head = temp.next;
+      temp.next = null
+      break;
+    }else{
+      temp = temp.next;
+      counter++;
+    }
+  }
+  return head;
+}
+
+let print = function(head){
+  while(head != null){
+    console.log(head.data);
+    head = head.next;
+  }
+}
+
+debugger
+const arr = [1,2,3,4,5];
+let head = linkedList(arr);
+head = rotateRight(head , 2);
+print(head);
