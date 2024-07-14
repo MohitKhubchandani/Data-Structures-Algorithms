766e35235bfe7c9ff83e9005322a334b31b8206d
Mohit04 <mohitkhubchandani88@gmail.com>
2024-07-14 16:07:55 +0530

    Added is empty

---
+++ Stacks/isEmpty.js
@@ -0,0 +1,28 @@
+class Stack{
+  #arr
+  constructor() {
+    this.#arr = []
+  }
+
+  push(element){
+    this.#arr.push(element)
+  }
+
+  pop(element){
+    this.#arr.pop(element)
+  }
+
+  top(element){
+    return this.#arr[this.#arr.length - 1];
+  }
+
+  isEmpty(){
+    return this.#arr.length == 0;
+  }
+}
+
+let stk = new Stack();
+stk.push(10);
+console.log(stk.isEmpty());
+stk.pop()
+console.log(stk.isEmpty()); // Output :- false true
