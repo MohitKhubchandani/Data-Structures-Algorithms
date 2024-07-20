69ab1e65ad65ef6449dcad8be5b2fd259d316a76
Mohit04 <mohitkhubchandani88@gmail.com>
2024-07-20 16:29:36 +0530

    Added binary Tree

---
+++ trees/binaryTree.js
@@ -0,0 +1,13 @@
+class Node{
+  constructor(key) {
+    this.vaue = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let root = new Node(10)
+root.left = new Node(20)
+root.right = new Node(30)
+root.right.left = new Node(50)
+console.log(root);
