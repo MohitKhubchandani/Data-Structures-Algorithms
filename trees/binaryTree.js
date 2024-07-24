01941b074d47fe38355a13e4246dc511ccc241c2
Mohit04 <mohitkhubchandani88@gmail.com>
2024-07-24 12:21:08 +0530

    Updated binary tree

--- trees/binaryTree.js
+++ trees/binaryTree.js
@@ -6,8 +6,15 @@ class Node{
   }
 }
 
+function binaryTree(root){
+  if(root == null)return 0;
+  console.log(root.value);
+  let left = binaryTree(root.left)
+  let right = binaryTree(root.right)  
+}
+
 let root = new Node(10)
 root.left = new Node(20)
 root.right = new Node(30)
 root.right.left = new Node(50)
-console.log(root.value);
+binaryTree(root); // Output :- 10 20 30 50 
