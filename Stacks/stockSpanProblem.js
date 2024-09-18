5223b2e73104a37ff9413f6d8cd0c5c368a4b3dd
Mohit04 <mohitkhubchandani88@gmail.com>
2024-09-18 14:59:16 +0530

    Added stock Span problem

---
+++ Stacks/stockSpanProblem.js
@@ -0,0 +1,31 @@
+class Stack{
+  #arr
+  constructor() {
+    this.#arr = []
+  }
+
+  push(element){
+    this.#arr.push(element)
+  }
+
+  pop(element){
+    this.#arr.pop(element)
+  }
+
+  top(element){
+    return this.#arr[this.#arr.length - 1];
+  }
+
+  isEmpty(){
+    return this.#arr.length == 0;
+  }
+}
+
+var StockSpanner = function() {
+
+};
+
+
+StockSpanner.prototype.next = function(price) {
+    
+};
