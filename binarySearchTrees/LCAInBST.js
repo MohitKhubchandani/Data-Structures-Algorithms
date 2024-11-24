cda0fa60726f5ab849db01dc76cec09c0a6937ae
Mohit04 <mohitkhubchandani88@gmail.com>
2024-11-25 05:24:22 +0530

    Updated Test case

--- binarySearchTrees/LCAInBST.js
+++ binarySearchTrees/LCAInBST.js
@@ -7,28 +7,32 @@ class TreeNode {
  };
 
 function lowestCommonAncestor(root, p, q) {
-    if (root.val > p.val && root.val > q.val) {
+    if (root == null) {
+        return null;
+    };
+    let cur = root
+
+    if (cur.val > p.val && cur.val > q.val) {
         return lowestCommonAncestor(root.left, p, q);
     }
-    else if (root.val < p.val && root.val < q.val) {
+    if (cur.val < p.val && cur.val < q.val) {
         return lowestCommonAncestor(root.right, p, q);
     }
-    else {
         return root.val;
-}};
+};
 
 
-const root = new TreeNode(3);
-root.left = new TreeNode(5);
-root.right = new TreeNode(1);
-root.left.left = new TreeNode(6);
-root.left.right = new TreeNode(2);
-root.left.right.left = new TreeNode(7);
-root.left.right.right = new TreeNode(4);
-root.right.left = new TreeNode(0);
-root.right.right = new TreeNode(8);
+const root = new TreeNode(6);
+root.left = new TreeNode(2);
+root.right = new TreeNode(8);
+root.left.left = new TreeNode(0);
+root.left.right = new TreeNode(4);
+root.left.right.left = new TreeNode(3);
+root.left.right.right = new TreeNode(5);
+root.right.left = new TreeNode(7);
+root.right.right = new TreeNode(9);
 
-const p = root.right;
-const q = root.left;
+const p = root.left;
+const q = root.left.right;
 const Output = lowestCommonAncestor(root, p, q);
-console.log(Output) // Output :- 3
+console.log(Output) // Output :- 2
