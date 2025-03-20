ecf16a366d6675ffa1a528401f9e354df8ce405a
Mohit04 <mohitkhubchandani88@gmail.com>
2025-03-21 04:27:33 +0530

    Added graph adjacency

---
+++ revision/graphAdjacency.js
@@ -0,0 +1,22 @@
+function graph(edges, n){
+    let adjecencyList = [];
+
+    for (let i = 0; i < edges.length - 2; i++) {
+        adjecencyList.push([]);
+    };
+
+    for (let i = 0; i < edges.length - 1; i++) {
+        let w = edges[i][0];
+        let v = edges[i][1];
+        
+        adjecencyList[w].push(v)
+        adjecencyList[v].push(w)
+    };
+
+    return adjecencyList;
+};
+
+const edges = [[0,1],[0,3],[1,2],[1,3],[2,4],[3,4],[4,5]];
+const n = edges.length - 1;
+const Output = graph(edges, n);
+console.log(Output) // Output :- [ [ 1, 3 ], [ 0, 2, 3 ], [ 1, 4 ], [ 0, 1, 4 ], [ 2, 3 ] ]
